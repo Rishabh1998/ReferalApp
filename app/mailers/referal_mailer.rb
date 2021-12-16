aa87ac875432948e0c5a32352104faa61c2d2646
Rishabh <aggarwalrishabh105@gmail.com>
2021-12-16 16:06:10 +0530

    fixed

--- app/mailers/referal_mailer.rb
+++ app/mailers/referal_mailer.rb
@@ -3,6 +3,7 @@ class ReferalMailer < ActionMailer::Base
 
     def send_referal_email(user, referal_user)
         @user = user
+        @id = @user.id
         @referal_user = referal_user
         mail(to: @referal_user, from: "smtponly10@gmail.com", subject: "Referal Email") do |format|
             format.html {render 'send_referal_email'}
