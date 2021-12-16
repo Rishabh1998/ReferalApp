391475c8b9b0fcb98d2bc0c1305dc78599485c49
Rishabh <aggarwalrishabh105@gmail.com>
2021-12-16 15:43:59 +0530

    fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,6 @@ class ApplicationController < ActionController::Base
     def authenticate
         if request.headers['token'].present?
             token = request.headers['token']
-            puts "token: #{token}"
             begin
                 payload = JWT.decode(token, 'c880a82c45b11015ee1b244c01628b94cb61c6a14672a5be42a3f4db26bb3e985f5944773655971b987e46604ad7643987ad7ff946c887d8841dc62b9e461ee0').first
                 @current_user = User.find(payload['id'])
