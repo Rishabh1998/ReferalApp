cc44e925ef6bad1ee1cd5afb82089786a28732dc
Rishabh <aggarwalrishabh105@gmail.com>
2021-12-16 15:35:36 +0530

    email fixes

--- app/mailers/referal_mailer.rb
+++ app/mailers/referal_mailer.rb
@@ -4,8 +4,8 @@ class ReferalMailer < ActionMailer::Base
     def send_referal_email(user, referal_user)
         @user = user
         @referal_user = referal_user
-        mail(to: @referal_user, subject: "Referal Email") do
-            render 'referal_email'
+        mail(to: @referal_user, from: "smtponly10@gmail.com", subject: "Referal Email") do |format|
+            format.html {render 'send_referal_email'}
         end
     end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -120,7 +120,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              "smtp.gmail.com",
-    port:                 "587",
+    port:                 587,
     user_name:            "smtponly10@gmail.com",
     password:             "Password@1234",
     authentication:       :login,
